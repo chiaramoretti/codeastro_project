37abb18138628de2eb94e29eebd980966d889dc8
ie-lopez <lopezeivan@gmail.com>
2022-06-22 17:53:07 +0200

    adding Image class and load_image()

---
+++ test/test.py
@@ -0,0 +1,33 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.table import QTable
+#from photutils.datasets import load_star_image
+from photutils.datasets import make_gaussian_sources_image
+from photutils.datasets import make_noise_image
+
+class Image():
+    def __init__(self, shape):
+        self.shape=shape
+
+    def load_image(self, n_sources):
+        table = QTable()
+        table['amplitude'] = np.random.rand(n_sources)*100
+        table['x_mean'] = np.random.rand(n_sources)*self.shape
+        table['y_mean'] = np.random.rand(n_sources)*self.shape
+        table['x_stddev'] = np.random.rand(n_sources)
+        table['y_stddev'] = np.random.rand(n_sources)
+        table['theta'] = np.radians(np.random.rand(n_sources)*360)
+        source= make_gaussian_sources_image(self.shape, table)
+        image = source +  make_noise_image(self.shape, distribution='gaussian', mean=5., stddev=5.)
+        image = image + make_noise_image(self.shape, distribution='poisson', mean=5.)
+        return image
+
+    def other_methods():
+        return results
+
+#%%
+shape=(100,100)
+array=Image(shape).load_image(n_sources=5)
+plt.imshow(array, origin='lower')
+#%%
